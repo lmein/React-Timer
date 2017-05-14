bdcada780fe309885206e63b0858fadcdfcea6cb
lmein <lmeinecke@gmail.com>
2017-05-14 11:31:27 -0500

    Add logic to handle countdown completion.

--- app/components/controls.jsx
+++ app/components/controls.jsx
@@ -14,17 +14,13 @@ var Controls = React.createClass({
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
-         //
         return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
       } else if (countdownStatus === 'paused') {
-         //
         return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
 
     return (
-        //
-                //
       <div className="controls">
         {renderStartStopButton()}
         <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
--- app/components/countdown.jsx
+++ app/components/countdown.jsx
@@ -26,12 +26,19 @@ var Countdown = React.createClass({
       }
     }
   },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function () {
     this.timer = setInterval(() => {
       var newCount = this.state.count - 1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   },
   handleSetCountdown: function (seconds) {
@@ -46,8 +53,6 @@ var Countdown = React.createClass({
   render: function () {
     var {count, countdownStatus} = this.state;
 
-    // console.log(countdownStatus);
-    // debugger;
     var renderControlArea = () => {
       if (countdownStatus !== 'stopped') {
         return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
